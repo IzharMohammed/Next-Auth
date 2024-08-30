84afcef0bbc85c7068f3c0e53549af74159fccfe
Mohammed Izhar <122259877+IzharMohammed@users.noreply.github.com>
2024-08-30 13:00:51 +0530

    Update route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
+import { NEXT_AUTH_CONFIG } from "@/app/lib/route";
 import nextAuth from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import CredentialsProvider from 'next-auth/providers/credentials';
-
 // export function GET(req: NextRequest, { params: { authRoutes } }: {
 //     params: {
 //         authRoutes: string[]
@@ -15,54 +14,8 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 //     })
 // }
 
-const handler = nextAuth({
-    providers: [
-        CredentialsProvider({
-            name: 'Credentials',
-            credentials: {
-                username: { label: 'email', type: 'text', placeholder: '' },
-                password: { label: 'password', type: 'password', placeholder: '' },
-            },
-            async authorize(credentials: any) {
-                console.log('credentials', credentials);
-
-                return {
-                    id: 'user1',
-                    name: 'izhar',
-                    email: 'izhar@test.com'
-                }
-            }
-        })
-    ],
-    secret: process.env.NEXTAUTH_SECRET,
-    callbacks: {
-        jwt: async ({ user, token }: any) => {
-            console.log('user', user);
-            console.log('token', token);
-            token.userId = token.sub
-            return token;
-        },
-        session: ({ session, token, user }: any) => {
-            console.log('session', session)
-            if (session.user) {
-                session.user.id = token.userId
-            }
-            return session;
-        }
-    }
-})
+const handler = nextAuth(NEXT_AUTH_CONFIG)
 
 
 export const GET = handler;
 export const POST = handler;
-
-
-
-
-
-
-
-
-
-
-
